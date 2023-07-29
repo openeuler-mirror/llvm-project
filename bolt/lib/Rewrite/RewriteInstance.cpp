7a0759b59ebacd1d898352585798cdb032b92306
Job Noorman <jnoorman@igalia.com>
2023-07-29 09:14:44 +0200

    [Backport][BOLT][RISCV] Recognize mapping symbols
    The RISC-V psABI [1] defines them similarly to AArch64.
    
    [1] https://github.com/riscv-non-isa/riscv-elf-psabi-doc/blob/master/riscv-elf.adoc#mapping-symbol
    
    Reviewed By: yota9, Amir
    
    Differential Revision: https://reviews.llvm.org/D153277

--- bolt/lib/Rewrite/RewriteInstance.cpp
+++ bolt/lib/Rewrite/RewriteInstance.cpp
@@ -882,7 +882,7 @@ void RewriteInstance::discoverFileObjects() {
         }
       };
 
-  if (BC->isAArch64()) {
+  if (BC->isAArch64() || BC->isRISCV()) {
     addExtraDataMarkerPerSymbol(SortedFileSymbols, SortedMarkerSymbols);
     LastSymbol = std::stable_partition(
         SortedFileSymbols.begin(), SortedFileSymbols.end(),
