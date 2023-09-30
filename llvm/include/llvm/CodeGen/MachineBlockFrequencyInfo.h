2c3b6e896c3a2a71de2a603cac373746079163bc
Mircea Trofin <mtrofin@google.com>
2023-09-29 18:06:53 -0700

    [AsmPrint] Correctly factor function entry count when dumping MBB frequencies (#67826)
    The goal in #66818 was to capture function entry counts, but those are not the same as the frequency of the entry (machine) basic block. This fixes that, and adds explicit profiles to the test.
    
    We also increase the precision of `MachineBlockFrequencyInfo::getBlockFreqRelativeToEntryBlock` to double. Existing code uses it as float so should be unaffected.

--- llvm/include/llvm/CodeGen/MachineBlockFrequencyInfo.h
+++ llvm/include/llvm/CodeGen/MachineBlockFrequencyInfo.h
@@ -65,8 +65,10 @@ public:
 
   /// Compute the frequency of the block, relative to the entry block.
   /// This API assumes getEntryFreq() is non-zero.
-  float getBlockFreqRelativeToEntryBlock(const MachineBasicBlock *MBB) const {
-    return getBlockFreq(MBB).getFrequency() * (1.0f / getEntryFreq());
+  double getBlockFreqRelativeToEntryBlock(const MachineBasicBlock *MBB) const {
+    assert(getEntryFreq() != 0 && "getEntryFreq() should not return 0 here!");
+    return static_cast<double>(getBlockFreq(MBB).getFrequency()) /
+           static_cast<double>(getEntryFreq());
   }
 
   std::optional<uint64_t>
