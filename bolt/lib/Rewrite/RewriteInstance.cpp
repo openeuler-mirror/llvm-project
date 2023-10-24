53a36b41f24935be10295ce8e6df44fbf71302cd
maksfb <maks@fb.com>
2023-10-24 12:22:43 -0700

    [Backport][BOLT] Fix incorrect basic block output addresses (#70000)
    Some optimization passes may duplicate basic blocks and assign the same
    input offset to a number of different blocks in a function. This is done
    e.g. to correctly map debugging ranges for duplicated code.
    
    However, duplicate input offsets present a problem when we use
    AddressMap to generate new addresses for basic blocks. The output
    address is calculated based on the input offset and will be the same for
    blocks with identical offsets. The result is potentially incorrect debug
    info and BAT records.
    
    To address the issue, we have to eliminate the dependency on input
    offsets while generating output addresses for a basic block. Each block
    has a unique label, hence we extend AddressMap to include address lookup
    based on MCSymbol and use the new functionality to update block
    addresses.

--- bolt/lib/Rewrite/RewriteInstance.cpp
+++ bolt/lib/Rewrite/RewriteInstance.cpp
@@ -3184,9 +3184,6 @@ void RewriteInstance::preregisterSections() {
                               ROFlags);
   BC->registerOrUpdateSection(getNewSecPrefix() + ".rodata.cold",
                               ELF::SHT_PROGBITS, ROFlags);
-  BC->registerOrUpdateSection(AddressMap::SectionName, ELF::SHT_PROGBITS,
-                              ROFlags)
-      .setLinkOnly();
 }
 
 void RewriteInstance::emitAndLink() {
@@ -3657,11 +3654,8 @@ void RewriteInstance::mapAllocatableSections(
 }
 
 void RewriteInstance::updateOutputValues(const BOLTLinker &Linker) {
-  if (auto MapSection = BC->getUniqueSectionByName(AddressMap::SectionName)) {
-    auto Map = AddressMap::parse(MapSection->getOutputContents(), *BC);
-    BC->setIOAddressMap(std::move(Map));
-    BC->deregisterSection(*MapSection);
-  }
+  if (std::optional<AddressMap> Map = AddressMap::parse(*BC))
+    BC->setIOAddressMap(std::move(*Map));
 
   for (BinaryFunction *Function : BC->getAllBinaryFunctions())
     Function->updateOutputValues(Linker);
