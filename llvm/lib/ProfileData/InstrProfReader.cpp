f202ece446ffb5eaaaf861fb7c01ef8933dbb041
Ellis Hoag <ellis.sparky.hoag@gmail.com>
2023-11-20 09:25:33 -0600

    [InstrProf] Add pgo use block coverage test (#72443)
    Back in https://reviews.llvm.org/D124490 we added a block coverage mode
    that instruments a subset of basic blocks using single byte counters to
    get coverage for the whole function.
    
    This commit adds a test to make sure that we correctly assign branch
    weights based on the coverage profile.
    
    I noticed this test was missing after seeing that we had no coverage on
    `PGOUseFunc::populateCoverage()`
    
    https://lab.llvm.org/coverage/coverage-reports/coverage/Users/buildslave/jenkins/workspace/coverage/llvm-project/llvm/lib/Transforms/Instrumentation/PGOInstrumentation.cpp.html#L1383

--- llvm/lib/ProfileData/InstrProfReader.cpp
+++ llvm/lib/ProfileData/InstrProfReader.cpp
@@ -270,6 +270,8 @@ Error TextInstrProfReader::readHeader() {
       ProfileKind |= InstrProfKind::FunctionEntryInstrumentation;
     else if (Str.equals_insensitive("not_entry_first"))
       ProfileKind &= ~InstrProfKind::FunctionEntryInstrumentation;
+    else if (Str.equals_insensitive("single_byte_coverage"))
+      ProfileKind |= InstrProfKind::SingleByteCoverage;
     else if (Str.equals_insensitive("temporal_prof_traces")) {
       ProfileKind |= InstrProfKind::TemporalProfile;
       if (auto Err = readTemporalProfTraceData())
