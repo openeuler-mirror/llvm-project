82d51aca563c40c84b70c0e295a9561d3dd4092b
jianghaibo <jianghaibo9@huawei.com>
2024-11-20 16:13:04 +0800

    [Driver] fix compile error for -fGNU-compatibility

--- clang/test/Driver/test-generate-missing-build-notes.cpp
+++ clang/test/Driver/test-generate-missing-build-notes.cpp
@@ -1,6 +1,7 @@
 // REQUIRES: build_for_openeuler
 // RUN: %clang -### -fgcc-compatible -Wa,--generate-missing-build-notes=yes %s 2>&1 | FileCheck -check-prefix=CHECK-NO-ERROR %s
 // RUN: %clang -### -fgcc-compatible -Wa,--generate-missing-build-notes=no %s 2>&1 | FileCheck -check-prefix=CHECK-NO-ERROR %s
+// RUN: %clang -### -fGNU-compatibility -Wa,--generate-missing-build-notes=no %s 2>&1 | FileCheck -check-prefix=CHECK-NO-ERROR %s
 // CHECK-NO-ERROR-NOT: --generate-missing-build-notes=
 // RUN: %clang -### -Wa,--generate-missing-build-notes=yes %s 2>&1 | FileCheck -check-prefix=CHECK-ERROR %s
 // RUN: %clang -### -Wa,--generate-missing-build-notes=no %s 2>&1 | FileCheck -check-prefix=CHECK-ERROR %s
