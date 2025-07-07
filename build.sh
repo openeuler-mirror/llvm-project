c6055765d30db71a0a93af473b606320d99f3bfd
liyunfei <liyunfei33@huawei.com>
2025-07-07 10:22:35 +0800

    Remove terminfo dylib dependency for centos image build

--- build.sh
+++ build.sh
@@ -453,6 +453,7 @@ cmake $CMAKE_OPTIONS \
       -DLIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=ON \
       -DLIBCXX_ENABLE_ABI_LINKER_SCRIPT=ON \
       -DLIBOMP_INSTALL_ALIASES=OFF \
+      -DLLVM_ENABLE_TERMINFO=NO \
       -DENABLE_ACPO=$enable_acpo \
       $llvm_binutils_incdir \
       $verbose \
