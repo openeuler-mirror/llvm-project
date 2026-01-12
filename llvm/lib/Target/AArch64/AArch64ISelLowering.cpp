bbd79f1a81e72ce78fc5ba3327c06d3b741e0efb
Kerry McLaughlin <kerry.mclaughlin@arm.com>
2026-01-12 15:53:17 +0000

    [AArch64] Add support for range prefetch intrinsic (#170490)
    This patch adds support in Clang for the RPRFM instruction, by adding
    the following intrinsics:
    
    ```
    void __pldx_range(unsigned int *access_kind*, unsigned int retention_policy,
                                   signed int length*, unsigned int count, signed int stride,
                                   size_t reuse distance,  void const *addr);
    
    void __pld_range(unsigned int access_kind*, unsigned int retention_policy,
                                  uint64_t metadata, void const *addr);
    ```
    
    The `__ARM_PREFETCH_RANGE` macro can be used to test whether these
    intrinsics are implemented. If the RPRFM instruction is not available, this
    instruction is a NOP.
    
    This implements the following ACLE proposal:
    https://github.com/ARM-software/acle/pull/423

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -2473,6 +2473,7 @@ const char *AArch64TargetLowering::getTargetNodeName(unsigned Opcode) const {
     MAKE_CASE(AArch64ISD::TBNZ)
     MAKE_CASE(AArch64ISD::TC_RETURN)
     MAKE_CASE(AArch64ISD::PREFETCH)
+    MAKE_CASE(AArch64ISD::RANGE_PREFETCH)
     MAKE_CASE(AArch64ISD::SITOF)
     MAKE_CASE(AArch64ISD::UITOF)
     MAKE_CASE(AArch64ISD::NVCAST)
@@ -4823,6 +4824,19 @@ SDValue AArch64TargetLowering::LowerINTRINSIC_VOID(SDValue Op,
 
     return DAG.getNode(AArch64ISD::PREFETCH, DL, MVT::Other, Chain,
                        DAG.getTargetConstant(PrfOp, DL, MVT::i32), Addr);
+  }
+  case Intrinsic::aarch64_range_prefetch: {
+    SDValue Chain = Op.getOperand(0);
+    SDValue Addr = Op.getOperand(2);
+
+    unsigned IsWrite = Op.getConstantOperandVal(3);
+    unsigned IsStream = Op.getConstantOperandVal(4);
+    unsigned PrfOp = (IsStream << 2) | IsWrite;
+
+    SDValue Metadata = Op.getOperand(5);
+    return DAG.getNode(AArch64ISD::RANGE_PREFETCH, DL, MVT::Other, Chain,
+                       DAG.getTargetConstant(PrfOp, DL, MVT::i32), Addr,
+                       Metadata);
   }
   case Intrinsic::aarch64_sme_za_enable:
     return DAG.getNode(
