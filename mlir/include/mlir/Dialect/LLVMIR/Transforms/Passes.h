6e8803fe54a830d8ba434363c01520653963e7e6
Muneeb Khan <muneebanwar@gmail.com>
2026-02-26 21:02:10 +0800

    Add an LLVMIR transform pass to convert f8 types to int8 types in safe memory ops. These ops treat f8 as a memory unit of 8 bits. Converting them to i8 does not change any arithmetic semantics, so functional correctness is guaranteed. Since llvm doesn't have f8 types, converting the safe memory ops to i8 types resolves their legalizing.
    (cherry picked from commit 78e877e8775e68bc5104f40c8bdf025973f752dd)

--- mlir/include/mlir/Dialect/LLVMIR/Transforms/Passes.h
+++ mlir/include/mlir/Dialect/LLVMIR/Transforms/Passes.h
@@ -11,6 +11,7 @@
 
 #include "mlir/Dialect/LLVMIR/LLVMAttrs.h"
 #include "mlir/Dialect/LLVMIR/Transforms/AddComdats.h"
+#include "mlir/Dialect/LLVMIR/Transforms/LegalizeFloat8Types.h"
 #include "mlir/Dialect/LLVMIR/Transforms/LegalizeForExport.h"
 #include "mlir/Dialect/LLVMIR/Transforms/OptimizeForNVVM.h"
 #include "mlir/Dialect/LLVMIR/Transforms/RequestCWrappers.h"
