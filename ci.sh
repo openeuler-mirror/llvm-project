1980a6f4f6e12b819bbee23fdb83cd30e8d4c69c
eastb233 <xiezhiheng@huawei.com>
2026-04-07 20:17:13 +0800

    Support for skipping unchanged PR

--- ci.sh
+++ ci.sh
@@ -55,8 +55,65 @@ elif [ "$(arch)" == "x86_64" ]; then
   PARALLEL_JOBS="32"
 fi
 
-#   4.2 执行构建
+SKIP="0"
+JSON_FILE="./pr_status/pr${PR_ID}.json"
+COMMIT_FILE="pr${PR_ID}.json"
+STATUS="fail"
+TOKEN="NONE"
+
+if [ -n "$1" ]; then
+  TOKEN="$1"
+fi
+
+#   4.2 检查是否需要skip
+cd ${WORKSPACE}/llvm-project
+COMMIT_ID=$(git log --pretty=format:%H | tail -1)
+
+rm -rf ./pr_status
+git clone --branch main https://eastb233:${TOKEN}@atomgit.com/eastb233/pr_status.git
+if [ -f "${JSON_FILE}" ]; then
+  JSON_SOURCE_BRANCH=$(jq -r '.source_branch' ${JSON_FILE})
+  JSON_DEST_BRANCH=$(jq -r '.dest_branch' ${JSON_FILE})
+  JSON_AUTHOR=$(jq -r '.author' ${JSON_FILE})
+  JSON_COMMIT_ID=$(jq -r '.commit_id' ${JSON_FILE})
+  JSON_STATUS=$(jq -r '.status' ${JSON_FILE})
+
+  if [ "${JSON_SOURCE_BRANCH}" == "${SOURCE_BRANCH}" ] && \
+     [ "${JSON_DEST_BRANCH}" == "${DEST_BRANCH}" ] && \
+     [ "${JSON_AUTHOR}" == "${AUTHOR}" ] && \
+     [ "${JSON_COMMIT_ID}" == "${COMMIT_ID}" ] && \
+     [ "${JSON_STATUS}" == "success" ]; then
+    SKIP="1"
+  fi
+fi
+
+if [ "${SKIP}" == "1" ]; then
+  echo "PR已测试通过且无任何变更"
+  exit 0
+fi
+
+#   4.3 执行构建
 cd ${WORKSPACE}/llvm-project
 bash build.sh -v -r -t -b relwithdebinfo -E -i -I install-$(arch)-for-pr-${PR_ID} -j${PARALLEL_JOBS}
 RESULT=$?
+if [ ${RESULT} -eq 0 ]; then
+  STATUS="success"
+fi
+
+cd ${WORKSPACE}/llvm-project/pr_status
+git config --local user.name eastb233
+git config --local user.email xiezhiheng@huawei.com
+cat > "${COMMIT_FILE}" <<EOF
+{
+  "pr_id": "${PR_ID}",
+  "source_branch": "${SOURCE_BRANCH}",
+  "dest_branch": "${DEST_BRANCH}",
+  "commit_id": "${COMMIT_ID}",
+  "status": "${STATUS}",
+  "timestamp": "$(date -Iseconds)"
+}
+EOF
+
+git add ${COMMIT_FILE} && git commit -m "CI adds PR${PR_ID} json" && git push origin main
+
 exit ${RESULT}
