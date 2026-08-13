4d81ec9bc1dabd0b6bb552b0d0ba36a4b4a2cd64
Chibuoyim (Wilson) Ogbonna <chibuoyim.faith.ogbonna@huawei.com>
2026-08-13 11:58:39 +0100

    [MLIR][Linalg] Recompute linalg.broadcast dimensions when flattening (#213641)
    per discussion in
    [211203](https://github.com/llvm/llvm-project/pull/211203), @Nujaa found
    the following case being rejected currently:
    ```
    func.func @broadcast_rank0_tensor(%arg0: tensor<i32>, %arg1: tensor<32x2xi32>) -> tensor<32x2xi32> {
      %0 = linalg.broadcast ins(%arg0 : tensor<i32>) outs(%arg1 : tensor<32x2xi32>) dimensions = [0, 1]
      return %0 : tensor<32x2xi32>
    }
    
    module attributes {transform.with_named_sequence} {
      transform.named_sequence @__transform_main(%arg1: !transform.any_op {transform.readonly}) {
        %0 = transform.structured.match interface{LinalgOp} in %arg1 : (!transform.any_op) -> !transform.any_op
        %flattened = transform.structured.flatten_elementwise %0
          : (!transform.any_op) -> !transform.any_op
        transform.yield
      }
    }
    ```
    with:
    ```
    ~/llvm-project/build$ bin/mlir-opt test.mlir --transform-interpreter
    test.mlir:7:8: error: 'linalg.broadcast' op input rank plus added dimensions does not match init rank. input rank: 0, dimensions size: 2, init rank: 1
      %0 = linalg.broadcast ins(%arg0 : tensor<i32>) outs(%arg1 : tensor<32x2xi32>) dimensions = [0, 1]
    ```
    After the `flatten_elementwise` pass runs, the `linalg.broadcast`
    *still* carries its original dimensions attribute `[0, 1]`, leaving us
    with something like:
    ```
    linalg.broadcast ins(%0 : tensor<i32>) outs(%1 : tensor<64xi32>) dimensions = [0, 1]
    ```
    which, of course, gets rejected by the `linalg.broadcast` verifier
    because `len(ins) + len(dimensions) != len(outs)`, as expected.
    
    Flattening/collapsing needs to also recompute the dimensions attribute
    for a broadcast, and this patch does this.
    
    ---------
    
    Co-authored-by: Andrzej Warzyński <andrzej.warzynski@gmail.com>
    (cherry picked from commit bb810316fb4cb8e1c3ebbee81467de62b24295aa)

--- mlir/lib/Dialect/Linalg/Transforms/ElementwiseOpFusion.cpp
+++ mlir/lib/Dialect/Linalg/Transforms/ElementwiseOpFusion.cpp
@@ -1662,13 +1662,33 @@ GenericOp cloneToCollapsedOp<GenericOp>(RewriterBase &rewriter,
   return collapsedOp;
 }
 
+/// Collapse a `BroadcastOp` with a 0-D input into the single flattened
+/// dimension (`dimensions = [0]`).
+template <>
+BroadcastOp
+cloneToCollapsedOp<BroadcastOp>(RewriterBase &rewriter, BroadcastOp origOp,
+                                const CollapsingInfo &collapsingInfo) {
+  assert(origOp.getInput().getType().getRank() == 0 && "expected a 0-D input");
+
+  SmallVector<Value> inputOperands, outputOperands;
+  SmallVector<Type> resultTypes;
+  collapseOperandsAndResults(origOp, collapsingInfo, rewriter, inputOperands,
+                             outputOperands, resultTypes);
+
+  SmallVector<int64_t> newDimensions = {0};
+  return rewriter.create<BroadcastOp>(origOp.getLoc(), inputOperands[0],
+                                      outputOperands[0], newDimensions);
+}
+
 LinalgOp createCollapsedOp(LinalgOp op, const CollapsingInfo &collapsingInfo,
                            RewriterBase &rewriter) {
   if (GenericOp genericOp = dyn_cast<GenericOp>(op.getOperation())) {
     return cloneToCollapsedOp(rewriter, genericOp, collapsingInfo);
-  } else {
-    return cloneToCollapsedOp(rewriter, op, collapsingInfo);
   }
+  if (BroadcastOp broadcastOp = dyn_cast<BroadcastOp>(op.getOperation())) {
+    return cloneToCollapsedOp(rewriter, broadcastOp, collapsingInfo);
+  }
+  return cloneToCollapsedOp(rewriter, op, collapsingInfo);
 }
 
 /// Implementation of fusion with reshape operation by collapsing dimensions.
